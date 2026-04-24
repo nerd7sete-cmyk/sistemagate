0c2a4ac3f7ebd1fa1feee9331ce9baf4251a645d
nerd7sete-cmyk <nerd7sete@gmail.com>
2026-04-24 12:58:11 -0300

    Add files via upload

---
+++ gateway zeroonpay/auth.js
@@ -0,0 +1,22 @@
+﻿// 🔐 PROTEÇÃO FORTE
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  let logado = localStorage.getItem("logado");
+  let tipo = localStorage.getItem("tipo");
+
+  let permitido = document.body.getAttribute("data-tipo");
+
+  // NÃO LOGADO
+  if(!logado){
+    window.location.href = "login.html";
+    return;
+  }
+
+  // TIPO ERRADO
+  if(permitido && tipo !== permitido){
+    window.location.href = "login.html";
+    return;
+  }
+
+});
